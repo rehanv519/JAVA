57b9d35f0705373b5a634620c045c9b389251436
rehanv519 <rehanvalora123@gmail.com>
2025-01-10 12:52:34 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,73 @@
+public class conditional
+{
+    public static void main(String[] args)
+    {
+        if (20 > 18) 
+        {
+           System.out.println("20 is greater than 18");
+        }
+    int x = 20;
+    int y = 18;
+    if (x > y) {
+      System.out.println("x is greater than y");
+    }  
+    int time = 20;
+if (time < 18) {
+  System.out.println("Good day.");
+} else {
+  System.out.println("Good evening.");
+}
+int tim = 22;
+if (tim < 10) {
+  System.out.println("Good morning.");
+} else if (tim < 18) {
+  System.out.println("Good day.");
+} else {
+  System.out.println("Good evening.");
+}
+ int tme = 20;
+    if (tme < 18) {
+      System.out.println("Good day.");
+    } else {
+      System.out.println("Good evening.");
+    } 
+    int ime = 20;
+String result = (ime < 18) ? "Good day." : "Good evening.";
+System.out.println(result);
+
+ int day = 4;
+    switch (day) {
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;
+         default:
+        System.out.println("Invalid Day");
+    }
+
+
+
+
+
+
+
+
+    }
+}
