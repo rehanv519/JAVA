485648e60cabc87eac15be0ed7c87150629b8eff
rehanv519 <rehanvalora123@gmail.com>
2025-01-10 12:13:33 +0530

    Add files via upload

---
+++ ArithmeticOperation.java
@@ -0,0 +1,28 @@
+
+package java1;
+
+public class ArithmeticOperation {
+    
+public static void main(String[] args)
+{
+    int i,j,ans;
+    i = 20;
+    j = 10;
+    
+    ans = i + j;
+    System.out.println("Addition : " + ans);
+    ans = i - j;
+    System.out.println("Subtraction : " + ans);
+    ans = i * j;
+    System.out.println("Multiplication : " + ans);
+    ans = i / j;
+    System.out.println("Division : " + ans);
+    ans = i  % j;
+    System.out.println("Modulus : " + ans);
+    
+    
+    
+}        
+        
+    
+}
